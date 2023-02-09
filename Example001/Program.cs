af5e89778ec6e2a9ffc99f7e258f11d3fba5c8e5
Vasya comp <vasja11420@gmail.com>
2023-02-09 21:58:10 +0500

    Пример 1

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 1; i <= n; i++)
+// {
+//     for (int j = 1; j <= n; j++)
+//     {
+//         int k = i * j;
+//         Console.SetCursorPosition(i*5, j);
+//         Console.Write(k);
+//     }
+
+// }
+
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = i; j <= n; j++)
+    {
+        int k = i * j;
+        Console.SetCursorPosition(i*5, j);
+        Console.Write(k);
+        Console.SetCursorPosition(j*5, i);
+        Console.Write(k);
+    }
+
+}
